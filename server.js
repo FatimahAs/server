17099f2548f96657f86bda576b32dac3a0867044
FATIMAH ASIRI <fatimahasiri@FATIMAHs-MacBook-Pro-2.local>
2025-06-15 13:47:11 +0300

    edit file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { createServer } from 'node:http';
 import fs from 'node:fs';
-const image = fs.readFileSync('cat.png')
 
+const image = fs.readFileSync('cat.png')
 const server = createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'image/png' });
     res.end(image);
@@ -10,3 +10,45 @@ const server = createServer((req, res) => {
 server.listen(3000, '127.0.0.1', () => {
     console.log('Listening on 127.0.0.1:3000');
 });
+
+//______________server html 'Content-Type'_______________
+
+//import { createServer } from 'node:http';
+
+//const html = `<!DOCTYPE html>
+//<html lang="en">
+//<head>
+//	<meta charset="UTF-8">
+//	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+//	<title>Document</title>
+//</head>
+//<body>
+//	<h1>hello world </h1>
+//</body>
+//</html>`
+
+//const server = createServer((req, res) => {
+//    res.writeHead(200, { 'Content-Type': 'text/html' });
+//    res.end(html);
+//});
+
+//server.listen(3000, '127.0.0.1', () => {
+//    console.log('Listening on 127.0.0.1:3000');
+//});
+
+// _________________redirect to outsourced image 'StatusCode'____________
+
+//import { createServer } from 'node:http';
+
+//const server = createServer((req, res) => {
+//	res.writeHead(303, {
+//		'Content-Type': 'text/plain', 
+//		"location" : 'https://www.happyhues.co/palettes/17'
+
+//	});
+//    res.end();
+//});
+
+//server.listen(3000, '127.0.0.1', () => {
+//    console.log('Listening on 127.0.0.1:3000');
+//});
